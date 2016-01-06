a31038bf77bbc93efecc25d1f7dded5ff76d7b88
orlmente <0rlm3nt3@gmail.com>
2016-01-06 10:42:54 +0100

    sh update

--- sass.sh
+++ sass.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
-if [ $1 == 'dev' ]
+if [ ! $# == 0 ]
     then
-    echo "compiling for developement"
-    sass $2 styles/main.scss:builds/assets/styles/style.css --line-numbers --sourcemap=none --style expanded $3
-    elif [ $1 == 'prod' ]
-    then
-    echo "compiling for production"
-    sass $2 styles/main.scss:builds/assets/styles/style.css $3
+    if [ $1 == 'dev' ]
+        then
+        echo "compiling for developement"
+        sass $2 styles/main.scss:builds/assets/styles/style.css --line-numbers --sourcemap=none --style expanded $3
+        elif [ $1 == 'prod' ]
+        then
+        echo "compiling for production"
+        sass $2 styles/main.scss:builds/assets/styles/style.css $3
+        else
+        echo "compiling with given switches"
+        sass styles/main.scss:builds/assets/styles/style.css $1
+    fi
     else
-    echo "compiling with given switches"
-    sass $1 styles/main.scss:builds/assets/styles/style.css $2
+    echo "Usage: sass.sh [env|watch-mode] [watch-mode] [other-switches]"
+    echo "to modify paths edit sass.sh"
 fi
